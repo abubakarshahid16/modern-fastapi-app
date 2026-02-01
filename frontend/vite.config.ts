864f12942bd07d64727e083b7ded03fc5514d71f
Abubakar Shahid <abubakarshahid16@gmail.com>
2026-02-01 16:27:16 +0500

    Fix: Deployment configuration and cleanup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,7 @@ import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
+
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
