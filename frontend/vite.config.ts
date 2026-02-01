f23b722523e4182dcf5193f40ec51feea56277c9
Abubakar Shahid <abubakarshahid16@gmail.com>
2026-02-01 16:27:52 +0500

    Fix: Vite configuration repair

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,9 @@ import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
-
+export default defineConfig({
+  base: '/modern-fastapi-app/',
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
